371e8c47acc734eb27d444173093f189bcd0bcba
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-14 07:06:55 -0500

    Add ActiveRecord Base to Console

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Flix
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    console do
+      ActiveRecord::Base.connection
+    end
   end
 end
