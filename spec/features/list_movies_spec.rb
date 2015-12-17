bad08cc0f38bfda6dac94e31bf25f893242af39a
Vadim Brodsky <vadimb@crankworks.ca>
2015-12-17 13:48:45 -0500

    Add additional expectations and make them pass

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -1,8 +1,13 @@
 require "spec_helper"
 
 describe 'Viewing the list of movies' do
+
   it 'shows the movies' do
     visit 'http://example.com/movies'
     expect(page).to have_text("3 Movies")
+    expect(page).to have_text("Movie 1")
+    expect(page).to have_text("Movie 2")
+    expect(page).to have_text("Movie 3")
   end
+
 end
