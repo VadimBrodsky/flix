b5bea36dc9c058da687eaf5dcab6e3b60556342e
Vadim Brodsky <vadimb@crankworks.ca>
2015-12-17 13:55:41 -0500

    Refactor the spec to use movies_url instead of hardcoded url

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe 'Viewing the list of movies' do
 
   it 'shows the movies' do
-    visit 'http://example.com/movies'
+    visit movies_url
     expect(page).to have_text("3 Movies")
     expect(page).to have_text("Movie 1")
     expect(page).to have_text("Movie 2")
