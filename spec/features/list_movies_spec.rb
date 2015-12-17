af85271fb28ae7c6e81e796de1050311e68dbcf3
Vadim Brodsky <vadimb@crankworks.ca>
2015-12-17 13:38:38 -0500

    Finish the first test

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -1,5 +1,8 @@
 require "spec_helper"
 
 describe 'Viewing the list of movies' do
-  it 'shows the movies'
+  it 'shows the movies' do
+    visit 'http://example.com/movies'
+    expect(page).to have_text("3 Movies")
+  end
 end
