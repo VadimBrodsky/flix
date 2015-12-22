87a2346d9c8c632acdd8689fe15803e64bae0152
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-22 07:25:49 -0500

    Add Movie Show action, route, view - make test pass

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   get 'movies' => 'movies#index'
+  get 'movies/:id' => 'movies#show', as: 'movie'
 end
--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -8,11 +8,11 @@ describe 'Viewing a movie listing' do
                       description: "Tony Stark builds an armored suit to fight the throes of evil",
                       released_on: "2008-05-02")
 
-    visit "http://www.example.com/movies/#{movie.id}"
+    visit "http://www.example.com/movies/#{movie1.id}"
 
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie1.rating)
-    expect(page).to have_text('$318,421,101.00')
+    expect(page).to have_text('$318,412,101.00')
     expect(page).to have_text(movie1.description)
     expect(page).to have_text(movie1.released_on)
 
