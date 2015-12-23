c5e763045982c0a047f94d60f8dc715c43c101ff
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-22 21:12:55 -0500

    Add specs for movie Flop feature

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -6,8 +6,21 @@ describe 'Viewing a movie listing' do
 
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie1.rating)
-    expect(page).to have_text('$300,000,000.00')
     expect(page).to have_text(movie1.description)
     expect(page).to have_text(movie1.released_on)
+  end
+
+  it 'shows the total gross if the total gross exceeds $50M' do
+    movie = Movie.create(movie_attributes(total_gross: 600_000_000.00))
+    visit movie_url(movie)
+
+    expect(page).to have_text('$600,000,000.00')
+  end
+
+  it "shows 'Flop!' if the total gross is less than $50M" do
+    movie = Movie.create(movie_attributes(total_gross: 20_000_000.00))
+    visit movie_url(movie)
+
+    expect(page).to have_text('Flop!')
   end
 end
