322809e51d4e9d82a25932479db9c50b06af8082
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-26 11:07:48 -0600

    Add update action, setup allowed parameters

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,18 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    # Restrict which attributes are allowed to be received from the form
+    # This is a security feature, to avoid mass assingment vulnerability
+    # .permit - specify the permitter attributes
+    # .require - will raise an exception if the given key isn't found
+    # .permit! - make all attributes updateable
+    movie_params = params.require(:movie).permit(
+      :title, :description, :rating, :released_on, :total_gross
+    )
+    @movie.update(movie_params)
+  end
 end
