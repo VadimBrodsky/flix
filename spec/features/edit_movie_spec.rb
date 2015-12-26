ae8a1d21d3c4eb6a26fd3acd6c580ca92f52badd
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-26 11:06:58 -0600

    Add spec for movie updating form

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -8,6 +8,11 @@ describe 'Editing a movie listing' do
 
     expect(current_path).to eq(edit_movie_path(movie))
     expect(find_field('Title').value).to eq(movie.title)
+
+    fill_in 'Title', with: 'Update Movie Title'
+    click_button 'Update Movie'
+    expect(current_path).to eq(movie_path(movie))
+    expect(page).to have_text('Update Movie Title')
   end
 
 end
