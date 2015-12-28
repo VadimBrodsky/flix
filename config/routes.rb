c1641a1a6f914e4d2c8529b23c0572aef6e38d6c
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-28 13:10:46 -0500

    Conver all of the manual routes to a route resource

--- config/routes.rb
+++ config/routes.rb
@@ -54,10 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root  'movies#index'
-  get   'movies' => 'movies#index'
-  post  'movies' => 'movies#create'
-  get   'movies/new' => 'movies#new', as: 'new_movie'
-  get   'movies/:id' => 'movies#show', as: 'movie'
-  get   'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
-  patch 'movies/:id' => 'movies#update'
+  resources :movies
 end
