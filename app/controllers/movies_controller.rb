2005b0a87b6b8c1ea910d9e3514be605eaefdfa8
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-28 13:02:57 -0500

    Add supporting controller actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,16 +13,29 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie)
+  end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+  private
+
+  def movie_params
     # Restrict which attributes are allowed to be received from the form
     # This is a security feature, to avoid mass assingment vulnerability
     # .permit - specify the permitter attributes
     # .require - will raise an exception if the given key isn't found
     # .permit! - make all attributes updateable
-    movie_params = params.require(:movie).permit(
+    params.require(:movie).permit(
       :title, :description, :rating, :released_on, :total_gross
     )
-    @movie.update(movie_params)
-    redirect_to movie_path(@movie)
   end
 end
