9ad7c7be3df8d440e4f0a5a9947822bde7951229
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-28 13:02:24 -0500

    Fix typos in spec

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -11,13 +11,13 @@ describe 'Creating a new movie' do
     fill_in 'Description', with: 'Superheroes saving the world form villains'
     fill_in 'Rating', with: 'PG-13'
     fill_in 'Total gross', with: '75000000'
-    select (Time.now.year - 1).to_s, from: 'movie_released_on_li'
+    select (Time.now.year - 1).to_s, from: 'movie_released_on_1i'
 
     # If you are taking advantage of the HTML5 date field in Chrome,
     # use the 'fill_in' rather than 'select'
     # fill_in 'Released on', with: (Time.now.year - 1).to_s
 
-    click_button 'Create movie'
+    click_button 'Create Movie'
 
     expect(current_path).to eq(movie_path(Movie.last))
     expect(page).to have_text('New Movie Title')
