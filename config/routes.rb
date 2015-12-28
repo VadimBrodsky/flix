1b554b0148c1eb64d56958566079bb16f5f151fc
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-28 13:05:13 -0500

    Revise new route to match REST pattern

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rails.application.routes.draw do
   root  'movies#index'
   get   'movies' => 'movies#index'
   post  'movies' => 'movies#create'
-  get   'movies/create' => 'movies#new', as: 'new_movie'
+  get   'movies/new' => 'movies#new', as: 'new_movie'
   get   'movies/:id' => 'movies#show', as: 'movie'
   get   'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
   patch 'movies/:id' => 'movies#update'
