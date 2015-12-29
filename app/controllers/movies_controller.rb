dc4705185e269df04bdbb2530ff8ccacf65d4f16
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-29 13:11:39 -0500

    Add destroy action to delete the record from the DB

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,6 +26,12 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
   private
 
   def movie_params
