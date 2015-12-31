89e6c5b1f138f62b19367e635f9bb2ec4e5c2c6b
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-30 23:37:31 -0500

    Change to single quotes

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -4,9 +4,9 @@ def movie_attributes(overrides = {})
     total_gross: 318_412_101.00,
     description: 'Tony Stark builds an armored suit to fight the throes of evil',
     released_on: '2008-05-02',
-    cast: "Robert Downey Jr., Gwyneth Paltrow and Terrence Howard",
-    director: "Jon Favreau",
-    duration: "126 min",
-    image_file_name: "ironman.jpg"
+    cast: 'Robert Downey Jr., Gwyneth Paltrow and Terrence Howard',
+    director: 'Jon Favreau',
+    duration: '126 min',
+    image_file_name: 'ironman.jpg'
   }.merge(overrides)
 end
