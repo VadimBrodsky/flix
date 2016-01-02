19e921a9d927a244a3e6d19a17d722f4da3132c4
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-02 11:20:04 -0500

    Add image_for helper to deal without poster image

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,12 @@ module MoviesHelper
       number_to_currency(movie.total_gross)
     end
   end
+
+  def image_for(movie)
+    if movie.image_file_name.blank?
+      image_tag('placeholder.png')
+    else
+      image_tag(movie.image_file_name)
+    end
+  end
 end
