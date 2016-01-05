430f02763f302a28467b97efa1cd1f0b099fd9ac
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-04 23:40:17 -0500

    Add Movie model validation rules

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,19 @@
 class Movie < ActiveRecord::Base
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, allow_blank: true, format:
+    {
+      with: /\w+\.(gif|jpg|png)\z/i,
+      message: 'must reference a GIF, JPG, or PNG image'
+    }
+  validates :rating, inclusion: { in: RATINGS }
+
   def flop?
-  total_gross.blank? || total_gross < 50_000_000
+    total_gross.blank? || total_gross < 50_000_000
   end
 
   def self.released
