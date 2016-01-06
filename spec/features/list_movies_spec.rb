6e016a51c4a80293432829389f06044df22a9f6b
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-05 22:46:50 -0500

    Update spec to pass validations

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -2,29 +2,15 @@ describe 'Viewing the list of movies' do
 
   it 'shows the movies' do
     # Arrange
-    movie1 = Movie.create(title: "Iron Man",
-                      rating: "PG-13",
-                      total_gross: 318412101.00,
-                      description: "Tony Stark builds an armored suit to fight the throes of evil",
-                      released_on: "2008-05-02")
-
-    movie2 = Movie.create(title: "Superman",
-                      rating: "PG",
-                      total_gross: 134218018.00,
-                      description: "Clark Kent grows up to be the greatest super-hero",
-                      released_on: "1978-12-15")
-
-    movie3 = Movie.create(title: "Spider-Man",
-                      rating: "PG-13",
-                      total_gross: 403706375.00,
-                      description: "Peter Parker gets bit by a genetically modified spider",
-                      released_on: "2002-05-03")
+    movie1 = Movie.create(movie_attributes(total_gross: 318_412_101.00))
+    movie2 = Movie.create(movie_attributes)
+    movie3 = Movie.create(movie_attributes)
 
     # Act
     visit movies_url
 
     # Assert
-    expect(page).to have_text("3 Movies")
+    expect(page).to have_text('3 Movies')
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie2.title)
     expect(page).to have_text(movie3.title)
