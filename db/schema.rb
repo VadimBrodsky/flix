17d15c4c9e07090ebcb4c5123664440e5e1da34a
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-07 13:52:38 -0500

    Generate the Review resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231043849) do
+ActiveRecord::Schema.define(version: 20160107184832) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -26,5 +26,16 @@ ActiveRecord::Schema.define(version: 20151231043849) do
     t.string   "duration"
     t.string   "image_file_name"
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "stars"
+    t.text     "comment"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
 
 end
