13c4302a1a657b860d8097dba59a0207704c846a
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-07 14:34:09 -0500

    Add cascade to delete the reviews if the movie is defelted

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,7 +12,7 @@ class Movie < ActiveRecord::Base
     }
   validates :rating, inclusion: { in: RATINGS }
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   def flop?
     total_gross.blank? || total_gross < 50_000_000
