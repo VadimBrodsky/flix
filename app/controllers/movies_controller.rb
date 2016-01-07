7411324b5c75966cb71f7a0f44961d424af345d9
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-07 07:17:31 -0500

    Redisplay the form if validation fails

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,8 +25,12 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create(movie_params)
-    redirect_to movie_path(@movie)
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
   end
 
   def destroy
