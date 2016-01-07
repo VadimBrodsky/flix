2afc458ba85d890d25585d4faf6c2f3d287d9f47
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-07 14:21:23 -0500

    Add Review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
+  STARS = [1, 2, 3, 4, 5]
+
+  validates :name, :comment, presence: true
+  validates :comment, length: { minimum: 4 }
+  validates :stars, inclusion: { in: STARS, message: 'must be between 1 and 5' }
+
   belongs_to :movie
 end
