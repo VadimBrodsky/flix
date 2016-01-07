b75fa790242a47b0a2f91d0e609971f801db81b1
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-07 11:28:30 -0500

    Add flash object to update action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
+      flash[:notice] = 'Movie successfully updated!'
       redirect_to movie_path(@movie)
     else
       render :edit
