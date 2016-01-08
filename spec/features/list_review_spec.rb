d4022960eaf5563159134a25ce630d6d2be72428
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-08 10:51:54 -0500

    Add review listing spec

---
+++ spec/features/list_review_spec.rb
@@ -0,0 +1,16 @@
+describe 'Viewing a list of review' do
+  it 'shows the reviews for a specific movie' do
+    movie1 = Movie.create(movie_attribures(title: 'Iron Man'))
+    review1 = movie1.reviews.create(review_attributes(name: 'Roger Ebert'))
+    review2 = movie1.reviews.create(review_attributes(name: 'Gene Siskel'))
+
+    movie2 = Movie.create(movie_attribures(title: 'Superman'))
+    review3 = movie2.reviews.create(review_attributes(name: 'Peter Travers'))
+
+    visit movie_reviews_url(movie1)
+
+    expect(page).to have_text(review1.name)
+    expect(page).to have_text(review2.name)
+    expect(page).not_to have_text(review3.name)
+  end
+end
