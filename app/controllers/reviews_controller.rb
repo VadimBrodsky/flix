81b44890d8a81d971b90085608f305201f8ca0c0
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-08 17:01:32 -0500

    DRY the get of the movie ID into a before action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
     if @review.save
       redirect_to movie_reviews_path(@movie), notice: 'Thanks for your review!'
@@ -23,5 +22,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:name, :comment, :stars)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
