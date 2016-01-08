8df9b417bd840a7830f5852d373a29c7f5a3a221
Vadim Brodsky <vadimb@crankworks.ca>
2016-01-08 16:46:03 -0500

    Adds create action with a private permitted params method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,4 +8,20 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    review = @movie.reviews.new(review_params)
+    if review.save
+      redirect_to movie_reviews_path(@movie), notice: 'Thanks for your review!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :comment, :stars)
+  end
 end
