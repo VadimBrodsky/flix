8b7d2614f3f663756dce700b4ba5d709b0415c35
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-09 16:22:24 -0500

    Changes format stars to display number of asteriks instead of a number

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -17,7 +17,8 @@ module MoviesHelper
 
   def format_average_stars(movie)
     if movie.reviews.any?
-      pluralize(number_with_precision(movie.average_stars, precision: 1), 'star')
+      # pluralize(number_with_precision(movie.average_stars, precision: 1), 'star')
+      '*' * movie.average_stars.round
     else
       content_tag(:strong, 'No Reviews')
     end
