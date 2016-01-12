130783fc139273bcf8bea0302c312115d549a07f
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-11 22:06:53 -0500

    Adds model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                    format: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i,
+                    uniqueness: { case_sensitive: false }
 end
