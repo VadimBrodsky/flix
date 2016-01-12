c9511d822ac2f84b113fbe8625fbf54e21c43864
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-11 21:41:04 -0500

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107194043) do
+ActiveRecord::Schema.define(version: 20160112022437) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -38,5 +38,13 @@ ActiveRecord::Schema.define(version: 20160107194043) do
   end
 
   add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
