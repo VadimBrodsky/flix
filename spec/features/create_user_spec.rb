ec8914a2ec336e36cf917b954099d22807aba92f
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-12 07:20:31 -0500

    Adds User create spec

---
+++ spec/features/create_user_spec.rb
@@ -0,0 +1,25 @@
+describe 'Creating a new user' do
+  it 'saves the user and shows the user\'s profile page' do
+    visit root_url
+    click_link 'Sign Up'
+
+    expect(current_path).to eq(signup_path)
+
+    fill_in 'Name', with: 'Example User'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'abracadabra'
+    fill_in 'Confirm Password', with: 'abracadabra'
+
+    click_button 'Create User'
+
+    expect(current_path).to eq(user_path(User.last))
+    expect(page).to have_text('Example User')
+    expect(page).to have_text('Thanks for signing up!')
+  end
+
+  it 'does not save the user if it\'s invalid' do
+    visit signup_url
+    expect { click_button 'Create User' }.not_to change(User, :count)
+    expect(page).to have_text('error')
+  end
+end
