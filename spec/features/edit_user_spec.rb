4d2c4cbc22216cc80db770b6d7b5544db2001825
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-16 14:58:12 -0500

    Adds edit user spec

---
+++ spec/features/edit_user_spec.rb
@@ -0,0 +1,27 @@
+describe 'Editing a user' do
+  it 'updated the user and shows the user\'s updated details' do
+    user = User.create!(user_attributes)
+    visit user_url(user)
+    click_link 'Edit Account'
+
+    expect(current_path).to eq(edit_user_path(user))
+    expect(find_field('Name').value).to eq(user.name)
+
+    fill_in 'Name', with: 'Updated User Name'
+    click_button 'Update Account'
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_text('Updated User Name')
+    expect(page).to have_text('Account successfully updated!')
+  end
+
+  it 'does not update the user if it\'s invalid' do
+    user = User.create!(user_attributes)
+    visit edit_user_path(user)
+
+    fill_in 'Name', with: ' '
+    click_button 'Update Account'
+
+    expect(page).to have_text('error')
+  end
+end
