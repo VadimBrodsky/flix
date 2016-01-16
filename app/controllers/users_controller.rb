ec5fb6d58627cab00ffa330c7ee55046e152d483
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-16 15:32:04 -0500

    Add user destroy action and markup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_url, alert: 'Account successfully deleted!'
+  end
+
   private
 
   def user_params
--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -1,13 +1,13 @@
 describe 'Deleting a user' do
   it 'destroys the user and redirects to the home page' do
-    user = User.new(user_attributes)
+    user = User.create!(user_attributes)
     visit user_path(user)
     click_link 'Delete Account'
 
     expect(current_path).to eq(root_path)
     expect(page).to have_text('Account successfully deleted!')
 
-    visit_users_path
+    visit users_path
     expect(page).not_to have_text(user.name)
   end
 end
