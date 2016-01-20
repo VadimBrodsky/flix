3da2aa349ed3d8ee4b54eae0fbd5c24bf846a1c5
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-19 23:28:11 -0500

    Add username field migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112022437) do
+ActiveRecord::Schema.define(version: 20160120041837) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160112022437) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "username"
   end
 
 end
