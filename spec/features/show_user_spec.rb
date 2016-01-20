af10749496cfee197d8db6dc1b5da19463c8b986
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-19 23:05:01 -0500

    Adds member since spec and implementation

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -7,4 +7,12 @@ describe 'Viewing a user\'s profile page' do
     expect(page).to have_text(user.name)
     expect(page).to have_text(user.email)
   end
+
+  it 'shows the user\'s member since date' do
+    user = User.create!(user_attributes)
+
+    visit user_url(user)
+
+    expect(page).to have_text('Member Since January 2016')
+  end
 end
