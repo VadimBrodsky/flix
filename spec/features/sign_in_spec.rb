3e10ee9ad4426ec688a9dc5fd0c7f82fd9014d88
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-20 15:33:05 -0500

    Add valid / invalid sign-in spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,8 +2,34 @@ describe 'Signing in' do
   it 'prompts for an email and password' do
     visit root_url
     click_link 'Sign In'
-    expect(current_path).to eq(new_session_path)
+    expect(current_path).to eq(signin_path)
     expect(page).to have_field('Email')
     expect(page).to have_field('Password')
+  end
+
+  it 'signs in the user if the email/password combination is valid' do
+    user = User.create!(user_attributes)
+    visit root_url
+    click_link 'Sign In'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it 'does not sign in the user if the email/password combination is invalid' do
+    user = User.create!(user_attributes)
+    visit root_url
+    click_link 'Sign In'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'no match'
+    click_button 'Sign In'
+
+    expect(current_path).to eq(signin_path)
+    expect(page).to have_text('Invalid')
   end
 end
