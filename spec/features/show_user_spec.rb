c5362b996faa180d77d83c657131a6537f7d5c71
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-20 21:50:10 -0500

    Update by signing in user

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -2,6 +2,7 @@ describe 'Viewing a user\'s profile page' do
   it 'shows the user\'s details' do
     user = User.create!(user_attributes)
 
+    sign_in(user)
     visit user_url(user)
 
     expect(page).to have_text(user.name)
@@ -11,6 +12,7 @@ describe 'Viewing a user\'s profile page' do
   it 'shows the user\'s member since date' do
     user = User.create!(user_attributes)
 
+    sign_in(user)
     visit user_url(user)
 
     expect(page).to have_text('Member Since January 2016')
