b936a375a054c986ed93d9f2c00d787284717bee
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-21 11:02:41 -0500

    Add Admin column to users, default to false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120041837) do
+ActiveRecord::Schema.define(version: 20160121160134) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -43,9 +43,10 @@ ActiveRecord::Schema.define(version: 20160120041837) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "username"
+    t.boolean  "admin",           default: false
   end
 
 end
