1cee9e4901be46ee8cd8eefdb9475db1acb8e09e
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-21 07:18:11 -0500

    DRY the actions, user lookup is in the before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: 'Account successfully updated!'
     else
@@ -38,7 +36,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to root_url, alert: 'Account successfully deleted!'
