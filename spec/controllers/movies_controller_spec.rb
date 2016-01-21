d944ab5f9ad8f28841e114ba5cd2efb4659328fb
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-21 11:30:37 -0500

    Add login to spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -6,29 +6,34 @@ describe MoviesController do
   end
 
   context 'when not signed in as an admin user' do
+    before do
+      @user = User.create!(user_attributes)
+      session[:user_id] = @user
+    end
+
     it 'cannot access new' do
       get :new
-      expect(response).to redirect_to(root_url)
+      expect(response).to redirect_to(root_path)
     end
 
     it 'cannot access create' do
       post :create
-      expect(response).to redirect_to(root_url)
+      expect(response).to redirect_to(root_path)
     end
 
     it 'cannot access edit' do
       get :edit, id: @movie
-      expect(response).to redirect_to(root_url)
+      expect(response).to redirect_to(root_path)
     end
 
     it 'cannot access update' do
       patch :update, id: @movie
-      expect(response).to redirect_to(root_url)
+      expect(response).to redirect_to(root_path)
     end
 
     it 'cannot access destroy' do
       delete :destroy, id: @movie
-      expect(response).to redirect_to(root_url)
+      expect(response).to redirect_to(root_path)
     end
   end
 end
