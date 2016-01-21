36862026171641c1903b71dda0107bfe8c81913f
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-21 12:25:44 -0500

    Only admins can delete user accounts

--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -1,21 +1,26 @@
 describe 'Deleting a user' do
+  before do
+    @admin = User.create!(user_attributes(admin: true))
+    @user = User.create!(user_attributes(name: 'John Smith',
+                                         email: 'user@example.com',
+                                         username: 'usr'))
+    sign_in(@admin)
+  end
+
   it 'destroys the user and redirects to the home page' do
-    user = User.create!(user_attributes)
-    sign_in(user)
-    visit user_path(user)
+    visit user_path(@user)
     click_link 'Delete Account'
 
     expect(current_path).to eq(root_path)
     expect(page).to have_text('Account successfully deleted!')
 
     visit users_path
-    expect(page).not_to have_text(user.name)
+    expect(page).not_to have_text(@user.name)
   end
 
   it 'automatically signs out that user' do
-    user = User.create!(user_attributes)
-    sign_in(user)
-    visit user_path(user)
+    skip 'Only admin can delete users'
+    visit user_path(@user)
 
     click_link 'Delete Account'
 
