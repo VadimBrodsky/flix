bc3ddf92f984b82be3e583055dda941a60024e3b
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-20 21:48:57 -0500

    Add basic authorization controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe UsersController do
+  before do
+    @user = User.create!(user_attributes)
+  end
 
+  context 'when not signed in' do
+    before do
+      session[:user_id] = nil
+    end
+
+    it 'cannot access index' do
+      get :index
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it 'cannot access show' do
+      get :show, id: @user
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it 'cannot access edit' do
+      get :edit, id: @user
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it 'cannot access update' do
+      patch :update, id: @user
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it 'cannot access destroy' do
+      delete :destroy, id: @user
+      expect(response).to redirect_to(new_session_url)
+    end
+  end
 end
