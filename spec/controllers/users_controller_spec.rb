536330e69c2629f80715638a95e9a49257daa902
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-21 07:10:47 -0500

    Add specs for correct user for edit update delete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,4 +35,28 @@ describe UsersController do
       expect(response).to redirect_to(new_session_url)
     end
   end
+
+  context 'when signed in as the wrong user' do
+    before do
+      @wrong_user = User.create!(user_attributes(email: 'wrong@example.com',
+                                                 username: 'wrong'))
+      session[:user_id] = @wrong_user
+    end
+
+    it 'cannot edit another user' do
+      get :edit, id: @user
+      expect(response).to redirect_to(root_url)
+    end
+
+    it 'cannot update another user' do
+      patch :update, id: @user
+      expect(response).to redirect_to(root_url)
+    end
+
+    it 'cannot destroy another user' do
+      delete :destroy, id: @user
+      expect(response).to redirect_to(root_url)
+      expect(page).not_to have_text('successfully')
+    end
+  end
 end
