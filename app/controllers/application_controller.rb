38076ac22220d71ed2e77e4ed85b1c572e484b09
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-20 22:05:04 -0500

    Add require_signin before action for authorization, move current_user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   add_flash_types(:danger)
+
+  private
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_path, alert: 'Please sign in first!'
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_signin, expect: [:new, :create]
+
   def index
     @users = User.all
   end
