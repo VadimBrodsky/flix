b8b3b00bf985bae423892dbbf3e2e9f691af84d1
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-22 07:25:30 -0500

    Changes to DB schema by adding / removing columns to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121160134) do
+ActiveRecord::Schema.define(version: 20160122121725) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -28,13 +28,13 @@ ActiveRecord::Schema.define(version: 20160121160134) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string   "name"
     t.integer  "stars"
     t.text     "comment"
     t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "location"
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
