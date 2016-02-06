f05d4bf107e5873654bb602626721b221491333e
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-06 13:20:21 -0500

    Associate the user with the current user on create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = @movie.reviews.new(review_params)
+    @review.user = current_user
     if @review.save
       redirect_to movie_reviews_path(@movie), notice: 'Thanks for your review!'
     else
@@ -40,7 +41,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:name, :comment, :stars)
+    params.require(:review).permit(:comment, :stars)
   end
 
   def set_movie
