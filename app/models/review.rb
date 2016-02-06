15b730d2360ebe63d6d3b36529cfa7eef7dca880
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-06 13:16:31 -0500

    Declare the association between reviews and users, modify validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ActiveRecord::Base
+  belongs_to :movie
+  belongs_to :user
+
+  validates :comment, length: { minimum: 4 }
+
   STARS = [1, 2, 3, 4, 5]
 
-  validates :name, :comment, presence: true
-  validates :comment, length: { minimum: 4 }
   validates :stars, inclusion: { in: STARS, message: 'must be between 1 and 5' }
-
-  belongs_to :movie
 end
