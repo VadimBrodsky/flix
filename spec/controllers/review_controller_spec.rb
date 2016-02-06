7a85b625feaa9a39a91bb42276e5fa18134b6bd8
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-06 13:06:55 -0500

    Change from url to path to avoid domain errors in Rspec

--- spec/controllers/review_controller_spec.rb
+++ spec/controllers/review_controller_spec.rb
@@ -12,17 +12,17 @@ describe ReviewsController do
 
     it 'cannot access index' do
       get :index, movie_id: @movie
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(new_session_path)
     end
 
     it 'cannot access new' do
       get :new, movie_id: @movie
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(new_session_path)
     end
 
     it 'cannot access create' do
       post :create, movie_id: @movie
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(new_session_path)
     end
   end
 end
