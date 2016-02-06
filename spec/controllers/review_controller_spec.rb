b05fcbf32be51d1f52eb3eb5d5b56b3e15a3808b
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-06 12:55:40 -0500

    Add specs requiring login before interacting with reviews

---
+++ spec/controllers/review_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe ReviewsController do
+  before do
+    @movie = Movie.create!(movie_attributes)
+  end
+
+  context 'when not signed in' do
+    before do
+      session[:user_id] = nil
+    end
+
+    it 'cannot access index' do
+      get :index, movie_id: @movie
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it 'cannot access new' do
+      get :new, movie_id: @movie
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it 'cannot access create' do
+      post :create, movie_id: @movie
+      expect(response).to redirect_to(new_session_url)
+    end
+  end
+end
