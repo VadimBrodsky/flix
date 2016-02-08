98bbf68368258787a0c6c43a8e44cafabdc51afc
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-08 07:14:03 -0500

    Revise image helper, add user image to reviews

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
-  def profile_image_for(user)
-    url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+  def profile_image_for(user, options={})
+    size = options[:size] || 80
+    url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
     image_tag(url, alt: user.name)
   end
 end
