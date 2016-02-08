11d09749d6cb68095bb35d5e822eabfa547b45b0
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-08 06:52:53 -0500

    Remove local user variable, pass spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -141,19 +141,18 @@ describe 'authenticate' do
   end
 
   it 'has reviews' do
-    user = User.new(user_attributes)
     movie1 = Movie.new(movie_attributes(title: 'Iron Man'))
     movie2 = Movie.new(movie_attributes(title: 'Superman'))
 
     review1 = movie1.reviews.new(stars: 5, comment: 'Two thumbs up!')
-    review1.user = user
+    review1.user = @user
     review1.save!
 
     review2 = movie2.reviews.new(stars: 3, comment: 'Cool')
-    review2.user = user
+    review2.user = @user
     review2.save!
 
-    expect(user.reviews).to include(review1)
-    expect(user.reviews).to include(review2)
+    expect(@user.reviews).to include(review1)
+    expect(@user.reviews).to include(review2)
   end
 end
