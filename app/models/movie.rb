e5d15543a6e7c89defe82c6dea1b43d6753a2b0a
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-09 21:50:41 -0500

    Add query for movie critics using the has_many association through: reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,6 +16,7 @@ class Movie < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
+  has_many :critics, through: :reviews, source: :user
 
   def self.released
     # Active Record Queries Examples:
