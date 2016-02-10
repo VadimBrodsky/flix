df9b94ad519358326a6b56302c9d48492293c604
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-02-09 22:53:10 -0500

    Add create action to favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.favorites.create!(user: current_user)
+
+    # or append to the through association
+    # @movie.fans << current_user
+
+    redirect_to @movie, notice: 'Thanks for fav\'ing'
+  end
 end
