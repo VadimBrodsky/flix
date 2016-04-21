8205cb15ceb6433ec283e8479537414ba7b9a222
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-21 06:19:41 -0400

    Add specs for the genre checkboxes

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -2,6 +2,10 @@ describe 'Creating a new movie' do
   before do
     admin = User.create!(user_attributes(admin: true))
     sign_in(admin)
+
+    @genre1 = Genre.create!(name: "Genre 1")
+    @genre2 = Genre.create!(name: "Genre 2")
+    @genre3 = Genre.create!(name: "Genre 3")
   end
 
   it 'saves the movie and shows the new movie\'s details' do
@@ -25,11 +29,17 @@ describe 'Creating a new movie' do
     fill_in 'Duration', with: '123 min'
     fill_in 'Image file name', with: 'movie.png'
 
+    check(@genre1.name)
+    check(@genre2.name)
+
     click_button 'Create Movie'
 
     expect(current_path).to eq(movie_path(Movie.last))
     expect(page).to have_text('New Movie Title')
     expect(page).to have_text('Movie successfully created!')
+    expect(page).to have_text(@genre1.name)
+    expect(page).to have_text(@genre2.name)
+    expect(page).not_to have_text(@genre3.name)
   end
 
   it 'does not save the movie if it\'s invalid' do
