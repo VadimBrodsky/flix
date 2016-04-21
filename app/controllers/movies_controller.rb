ac11f0b518eabb1633315257a1f8356f607cf2a3
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-20 23:07:37 -0400

    Add fave / unfave to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,10 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+
+    if current_user
+      @current_favorite = current_user.favorites.find_by(movie_id: @movie)
+    end
   end
 
   def edit
