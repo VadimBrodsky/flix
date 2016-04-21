2118c8b4355fd58d930383ea3f419a44f1531dbd
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-21 06:53:58 -0400

    Show genres on the movie show view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+    @genres = @movie.genres
 
     if current_user
       @current_favorite = current_user.favorites.find_by(movie_id: @movie)
