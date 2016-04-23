012126e38169b7e63e73a77d54d9715f66f93d7c
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-23 11:38:05 -0400

    Refactor page title to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title
+    if content_for?(:title)
+      "#{content_for(:title)} | Flix  a better IMDB"
+    else
+      "Flix  a better IMDB"
+    end
+  end
 end
