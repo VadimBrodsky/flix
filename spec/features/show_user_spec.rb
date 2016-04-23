aa2e18357b225b6f2a92fdbacaf728f5d9f6fc55
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-23 11:07:54 -0400

    Add user specific layout changes

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -17,4 +17,27 @@ describe 'Viewing a user\'s profile page' do
 
     expect(page).to have_text("Member Since #{Time.now.strftime('%B %Y')}")
   end
+
+  it 'shows the user\'s favorites in the sidebar' do
+    user = User.create!(user_attributes)
+
+    movie = Movie.create!(movie_attributes)
+    user.favourite_movies << movie
+
+    sign_in(user)
+    visit user_url(user)
+
+    within('aside#sidebar') do
+      expect(page).to have_text(movie.title)
+    end
+  end
+
+  it 'includes the user\'s name in the page title' do
+    user = User.create!(user_attributes)
+
+    sign_in(user)
+    visit user_url(user)
+
+    expect(page).to have_title("Flix - #{user.name}")
+  end
 end
