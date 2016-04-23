3738b8bccccccb485d9ba9caeabd7c1e026b299d
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-23 11:52:13 -0400

    Change page title helper to render title tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
   def page_title
-    if content_for?(:title)
+    title_string = if content_for?(:title)
       "#{content_for(:title)} | Flix  a better IMDB"
     else
       "Flix  a better IMDB"
     end
+    content_tag(:title, title_string)
   end
 
   def title(title)
