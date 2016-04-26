0fef255b355ded00fe05589a950ab52123832efc
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-26 00:49:55 -0400

    Add additional routing examples

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   resources :genres
   root 'movies#index'
 
-  get 'movies/filter/:scope' => 'movies#index', as: :filtered_movies
+  # use regex to constrain the scope names
+  # get 'movies/:scope' => 'movies#index', constraints: { scope: /hits|flops|upcoming|recent/ }, as: :filtered_movies
 
+  # use loop to dynamically create list of routes
+  # redirect to filter namespace
   %w(hits flops upcoming recent).each do |scope|
     get "movies/#{scope}" => redirect("movies/filter/#{scope}")
   end
+
+  get 'movies/filter/:scope' => 'movies#index', as: :filtered_movies
 
   resources :movies do
     resources :reviews
