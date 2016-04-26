408b275f9a973b3a79e21f45a786c82fc97e8fe2
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-25 22:34:22 -0400

    Convert named query method to a scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,27 +20,25 @@ class Movie < ActiveRecord::Base
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
 
-  def self.released
-    # Active Record Queries Examples:
-    # Movie.count
-    # Movie.all
-    # Movie.order('released_on')
-    # Movie.order('released_on asc')
-    # Movie.order(released_on: :asc)
-    # Movie.order(released_on: :desc)
-    # Movie.find(2)
-    # Movie.find_by(title: 'Iron Man')
-    # Movie.where(rating: 'PG-13')
-    # Movie.where.not(rating: 'PG-13')
-    # Movie.where.not(rating: ['PG', 'PG-13'])
-    # Movie.where('total_gross < 50000000')
-    # Movie.where('total_gross < ?', 50_000_000)
-    # Movie.where('released_on <= ?', Time.now)
-    # Movie.where('released_on <= ?', Time.now).count
-    # Movie.where('released_on <= ?', Time.now).order('released_on')
+  scope :released, -> { where('released_on <= ?', Time.now).order(released_on: :desc) }
 
-    where('released_on <= ?', Time.now).order(released_on: :desc)
-  end
+  # Active Record Queries Examples:
+  # Movie.count
+  # Movie.all
+  # Movie.order('released_on')
+  # Movie.order('released_on asc')
+  # Movie.order(released_on: :asc)
+  # Movie.order(released_on: :desc)
+  # Movie.find(2)
+  # Movie.find_by(title: 'Iron Man')
+  # Movie.where(rating: 'PG-13')
+  # Movie.where.not(rating: 'PG-13')
+  # Movie.where.not(rating: ['PG', 'PG-13'])
+  # Movie.where('total_gross < 50000000')
+  # Movie.where('total_gross < ?', 50_000_000)
+  # Movie.where('released_on <= ?', Time.now)
+  # Movie.where('released_on <= ?', Time.now).count
+  # Movie.where('released_on <= ?', Time.now).order('released_on')
 
   def self.hits
     where('total_gross > ?', 300_000_000).order(total_gross: :desc)
