02595d6b36a99900c53bd233fef2f25aaa38860d
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-04-25 23:11:16 -0400

    Add scope for reviews with days ago parameter

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,6 @@ class Review < ActiveRecord::Base
   STARS = [1, 2, 3, 4, 5]
 
   validates :stars, inclusion: { in: STARS, message: 'must be between 1 and 5' }
+
+  scope :past_n_days, -> (days) { where('created_at >= ?', days.days.ago) }
 end
